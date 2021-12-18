c8320847c232cdd4499a805c6b369383e2694301
MattKohn <kohney@hotmail.com>
2021-12-18 15:15:19 -0700

    Complete assignment

---
+++ src/components/Messages.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Messages() {
+  return (
+    <div>
+      <h5>Note to self:</h5>
+      <p>You're awesome.</p>
+    </div>
+  )
+}
+
+export default Messages;
